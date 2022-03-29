8cb0c4fc5713a6d5db0db94eb43a67f4469f525a
IreneGalca <54726113+IreneGalca@users.noreply.github.com>
2022-03-28 22:35:29 -0600

    new db conn

--- connect.php
+++ connect.php
@@ -1,9 +1,12 @@
 <?php
-//mysql://bf97f6dce3ca8f:b41d024b@us-cdbr-east-05.cleardb.net/heroku_4541ca0fb19daa6?reconnect=true
-$host = "127.0.0.1:4306";
-$user = "root";
-$pass = "";
-$db = "hermit-hub";
-
-$conn = mysqli_connect($host, $user, $pass, $db);
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
